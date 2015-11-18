6b6cd2d48caa6d46ac6d7295db0780a95e56b03e
michaelwalkoski <michaelwalkoskispam@gmail.com>
2015-11-18 15:26:31 -0800

    Create phpapp.php

---
+++ phpapp.php
@@ -0,0 +1,15 @@
+<?php
+if (isset($_POST['value1'])) $value1 = $_POST['value1'];
+if (isset($_POST['value2'])) $value2 = $_POST['value2'];
+$answer = $value1 * $value2;
+
+echo <<<_END
+<form method='post' action='newapp.php'>
+<strong>Calculate</strong>&nbsp;<input type='text' name='value1' value="$value1"/>&nbsp;*&nbsp;<input type='text' name='value2' value="$value2"/><br/>
+<input type='submit' value='Calculate'/>
+_END;
+?>
+
+<br/>
+The answer is:<input type="text" value="<?php echo round($answer)?>">
+</form>
