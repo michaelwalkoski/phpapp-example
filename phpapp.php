938c672778708650077e9f8d35600dd96fe69c36
michaelwalkoski <michaelwalkoskispam@gmail.com>
2015-11-18 21:21:46 -0800

    Update phpapp.php

--- phpapp.php
+++ phpapp.php
@@ -1,35 +1,15 @@
 <?php
-# Comments behind a hashtag are not parsed by the server.
-# They are handy for other people working with your code
-# or later when when you forget what you intended.
-
-# Here we are creating a variable to determine price of cookies.
-$chocolatechipprice = 3;
-$macadamianutprice = 4;
-
-# Cookie wuantities are being determined by user input.
-if (isset($_POST['chocolatechipqty'])) $chocolatechipqty = $_POST['chocolatechipqty'];
-if (isset($_POST['macadamianutqty'])) $macadamianutqty = $_POST['macadamianutqty'];
-
-# We are totaling the cookie's price by type.
-$chocolatechiptotal = $chocolatechipprice * $chocolatechipqty;
-$macadamianuttotal = $macadamianutprice * $macadamianutqty;
-
-# The total price is the total of chocolate chip cookies
-# plus the total price of macadamia nut cookies.
-$total = $chocolatechiptotal + $macadamianuttotal;
-
-# The name and value of the text boxes has been changed to
-# coordinate with the above variables.
+if (isset($_POST['value1'])) $value1 = $_POST['value1'];
+if (isset($_POST['value2'])) $value2 = $_POST['value2'];
+$answer = $value1 * $value2;
 echo <<<_END
 <form method='post' action='phpapp.php'>
-<strong>Calculate Your Cookie Order</strong><br/><br/>
-Chocolate Chip Cookies ($3 each):&nbsp;<input type='text' name='chocolatechipqty' value="$chocolatechipqty"/><br/>
-Macadamia Nut Cookies ($4 each):&nbsp;<input type='text' name='macadamianutqty' value="$macadamianutqty"/><br/><br/>
+<strong>Calculate</strong><br/><br/>
+<input type='text' name='value1' value="$value1"/>&nbsp;*&nbsp;<input type='text' name='value2' value="$value2"/><br/><br/>
 <input type='submit' value='Calculate'/>
 _END;
 ?>
 
 <br/><br/>
-Your total is:&nbsp;$<input type="text" value="<?php echo round($total)?>">
+The answer is:<input type="text" value="<?php echo round($answer)?>">
 </form>
