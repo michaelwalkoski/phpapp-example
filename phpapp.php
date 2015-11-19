9f17b5a45a114a632838611385eb66c06bddea9c
michaelwalkoski <michaelwalkoskispam@gmail.com>
2015-11-18 21:17:29 -0800

    Update phpapp.php

--- phpapp.php
+++ phpapp.php
@@ -1,11 +1,26 @@
 <?php
+# Comments behind a hashtag are not parsed by the server.
+# They are handy for other people working with your code
+# or later when when you forget what you intended.
+
+# Here we are creating a variable to determine price of cookies.
 $chocolatechipprice = 3;
 $macadamianutprice = 4;
+
+# Cookie wuantities are being determined by user input.
 if (isset($_POST['chocolatechipqty'])) $chocolatechipqty = $_POST['chocolatechipqty'];
 if (isset($_POST['macadamianutqty'])) $macadamianutqty = $_POST['macadamianutqty'];
+
+# We are totaling the cookie's price by type.
 $chocolatechiptotal = $chocolatechipprice * $chocolatechipqty;
 $macadamianuttotal = $macadamianutprice * $macadamianutqty;
+
+# The total price is the total of chocolate chip cookies
+# plus the total price of macadamia nut cookies.
 $total = $chocolatechiptotal + $macadamianuttotal;
+
+# The name and value of the text boxes has been changed to
+# coordinate with the above variables.
 echo <<<_END
 <form method='post' action='phpapp.php'>
 <strong>Calculate Your Cookie Order</strong><br/><br/>
